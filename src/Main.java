3cb893f9d136f03b7af9327fdd5994b8e6313107
AleksandraKosohova <mk34r@mail.ru>
2017-09-20 22:23:44 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 public class Main {
+    public static final String ERROR_FEW_ARGUMENTS = "Too few arguments";
     public static void main(String[] args) {
+
         try {
             System.out.println(StringAnagramChecker.isAnagram(args[0], args[1]));
         }
-        catch (ArrayIndexOutOfBoundsException|DifferentLengthException e) {
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(ERROR_FEW_ARGUMENTS);
+        }
+        catch (DifferentLengthException e) {
             e.printStackTrace();
         }
     }
