f3f005f13fc2009a85f2aeee0aa7d6421db3f606
AleksandraKosohova <mk34r@mail.ru>
2017-09-20 22:01:59 +0300

    Add files via upload

--- src/StringAnagramCheckerTest.java
+++ src/StringAnagramCheckerTest.java
@@ -3,12 +3,12 @@ import static org.junit.Assert.*;
 public class StringAnagramCheckerTest {
     @org.junit.Test
     public void isAnagram() throws Exception {
-        assertTrue(StringAnagramChecker.isAnagram("qweq", "qwqe"));
-        assertFalse(StringAnagramChecker.isAnagram("qqq", "sss"));
+        assertTrue(StringAnagramChecker.isAnagram("elvis", "lives"));
+        assertFalse(StringAnagramChecker.isAnagram("elvis", "kives"));
     }
 
     @org.junit.Test(expected = DifferentLengthException.class)
     public void isAnagramException() throws DifferentLengthException {
-        assert(StringAnagramChecker.isAnagram("qw eq", "qwqe"));
+        StringAnagramChecker.isAnagram("elvis", "live");
     }
 }
