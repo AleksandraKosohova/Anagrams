7272a4bff761bc7f9dd2af692b3128fed4fc8256
AleksandraKosohova <mk34r@mail.ru>
2017-09-20 22:24:27 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static final String ERROR_FEW_ARGUMENTS = "Too few arguments";
     public static void main(String[] args) {
-
         try {
             System.out.println(StringAnagramChecker.isAnagram(args[0], args[1]));
         }
